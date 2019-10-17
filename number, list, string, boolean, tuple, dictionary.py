8810eafe8e148df3a8d21ad841e4fc2565216559
PaulinaS94 <56578401+PaulinaS94@users.noreply.github.com>
2019-10-17 12:06:36 +0200

    Add files via upload

---
+++ number, list, string, boolean, tuple, dictionary.py
@@ -0,0 +1,14 @@
+number_int = 5
+number_float = 5.2
+string = 'kawa'
+bool = False
+list = [25, 230, 'zero', 'tak, 25']
+tuple = (0, 569, 'kawa', 'pies', 0, 'mleko')
+dict = {'Fender': 1, 'Gibson': 0, 'LTD': 2, 'Prs':235}
+
+print('My numbers: ', number_int, ',', number_float)
+print('My string: ', string)
+print('Boolean: ', bool)
+print('My list: ', list)
+print('My tuple: ', tuple)
+print('My guitar dictionary: ', dict)
